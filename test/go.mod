873a10fbf03a192f11e64571285f0f3bbdc8d270
OwlWorksInnovations <juanpistoris01@gmail.com>
2026-04-15 14:17:20 +0200

    Initial

---
+++ test/go.mod
@@ -0,0 +1,3 @@
+module github.com/OwlWorksInnovations/go-packages/test
+
+go 1.26.2
---
+++ test/test.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+// Take note of the function name startin with a capitial
+func PrintInput(input string) {
+	fmt.Printf("Test output: %s\n", input)
+}
